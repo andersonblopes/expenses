4a0eb413dac8bd6ee0a950869de759dbf9d67f05
Anderson Lopes <anderson.lopes.2010@gmail.com>
2020-04-19 17:07:55 +0200

    Solves the context path on external tomcat.

--- src/main/java/com/lopes/expenses/ExpensesApplication.java
+++ src/main/java/com/lopes/expenses/ExpensesApplication.java
@@ -2,8 +2,13 @@ package com.lopes.expenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 import java.util.Locale;
@@ -12,7 +17,7 @@ import java.util.Locale;
  * The type Expenses application.
  */
 @SpringBootApplication
-public class ExpensesApplication {
+public class ExpensesApplication extends SpringBootServletInitializer {
 
     /**
      * The entry point of application.
@@ -31,5 +36,34 @@ public class ExpensesApplication {
     @Bean
     public LocaleResolver localeResolver() {
         return new FixedLocaleResolver(new Locale("pt", "BR"));
+    }
+
+    /**
+     * The type Mvc config.
+     */
+    @Configuration
+    public static class MvcConfig extends WebMvcConfigurerAdapter {
+
+        /**
+         * Add view controllers.
+         *
+         * @param registry the registry
+         */
+        @Override
+        public void addViewControllers(ViewControllerRegistry registry) {
+            registry.addRedirectViewController("/", "/expense");
+        }
+
+    }
+
+    /**
+     * Configure spring application builder.
+     *
+     * @param application the application
+     * @return the spring application builder
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExpensesApplication.class);
     }
 }
